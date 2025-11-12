8654debd9cade9746768f3f2392eada194cbceda
AlaricMarzo <pierre.marzo.cics@ust.edu.ph>
2025-11-13 06:20:58 +0800

    FIX 1 RAILWAY

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,4 +37,4 @@ app.use((err, req, res, next) => {
 
 
 const PORT = process.env.PORT || 5050
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
