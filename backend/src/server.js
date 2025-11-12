08c38195c4a6ed9c8edece4dd789547dbc14408c
AlaricMarzo <pierre.marzo.cics@ust.edu.ph>
2025-11-13 06:49:28 +0800

    FIX 5 RAILWAY

--- backend/src/server.js
+++ backend/src/server.js
@@ -48,7 +48,7 @@ const __dirname = dirname(__filename)
 app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 
 // Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
 })
 
